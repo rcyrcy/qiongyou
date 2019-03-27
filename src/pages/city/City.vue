1a0d1c239f8e8eea77e5db845f731a6fdfe08295
rcyrcy <18345059439@163.com>
2019-03-27 19:41:32 +0800

    city-list 动态数据

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
-        <city-list></city-list>
-        <city-alphabet></city-alphabet>
+        <city-list :cities="cities" :hot="hotCities"></city-list>
+        <city-alphabet :cities="cities"></city-alphabet>
     </div>
 </template>
 
@@ -13,6 +13,7 @@ import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
 import CityAlphabet from './components/Alphabet'
+import {getCity} from "@/api/city"
 export default {
     name:'City',
     components:{
@@ -20,6 +21,21 @@ export default {
         CitySearch,
         CityList,
         CityAlphabet
+    },
+    data(){
+        return{
+            cities:{},
+            hotCities:[]
+        }
+    },
+    mounted(){
+        getCity().then(res => {
+            if(res.ret == true){
+                const data = res.data
+                this.cities = data.cities
+                this.hotCities = data.hotCities
+            }
+        })
     }
 }
 </script>
