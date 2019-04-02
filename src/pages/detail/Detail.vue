676fa08f9155bdd710338cd6ca5682fd5fab18c5
rcyrcy <18345059439@163.com>
2019-04-02 20:51:54 +0800

    detail-header finish

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,20 +1,29 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+    <keep-alive>
+      <detail-header></detail-header>
+    </keep-alive>
+    <div class="content"></div>
   </div>
 </template>
 
 
 <script>
 import DetailBanner from "./components/Banner";
+import DetailHeader from "./components/Header";
 export default {
   name: "Detail",
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 };
 </script>
 
 
 <style lang="stylus" scoped>
+.content {
+  height: 50rem;
+}
 </style>
---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
+      <div class="iconfont header-abs-icon">&#xe624;</div>
+    </router-link>
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+      <router-link to="/">
+        <div class="iconfont header-fixed-back">&#xe624;</div>
+      </router-link>景点详情
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "DetailHeader",
+  data() {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    };
+  },
+  methods: {
+    handleScroll() {
+      const top = document.documentElement.scrollTop;
+      if (top > 60) {
+        let opacity = top / 140;
+        opacity = opacity > 1 ? 1 : opacity;
+        this.opacityStyle = { opacity };
+        this.showAbs = false;
+      } else {
+        this.showAbs = true;
+      }
+    }
+  },
+  activated() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+};
+</script>
+
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+
+.header-abs {
+  position: absolute;
+  left: 0.2rem;
+  top: 0.2rem;
+  width: 0.6rem;
+  height: 0.6rem;
+  line-height: 0.6rem;
+  border-radius: 0.3rem;
+  background: rgba(0, 0, 0, 0.8);
+
+  .header-abs-icon {
+    color: #fff;
+    font-size: 0.3rem;
+    text-align: center;
+  }
+}
+
+.header-fixed {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  line-height: $headerHeight;
+  height: $headerHeight;
+  text-align: center;
+  color: #fff;
+  background: $bgColor;
+  font-size: 0.3rem;
+
+  .header-fixed-back {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 0.64rem;
+    text-align: center;
+    font-size: 0.4rem;
+    color: #fff;
+  }
+}
+</style>
